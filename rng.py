c82c32a69e82288dc9c1981974177ba9601ee823
m-atalla <mo.atalla96@gmail.com>
2022-09-06 22:56:32 +0200

    add rng module

---
+++ rng.py
@@ -0,0 +1,21 @@
+import random
+import const
+
+def ip():
+    a = u8_int()
+    b = u8_int()
+    c = u8_int()
+    d = u8_int()
+
+    ip = "{}.{}.{}.{}".format(a,b,c,d)
+
+    return (ip, key())
+
+def key() -> int:
+    return int(random.uniform(0, const.UPPER_BOUND))
+
+def u8_int():
+    return random.randint(0, 255)
+
+def finger_idx() -> int:
+    return random.randint(1, const.M_BITS - 1)
